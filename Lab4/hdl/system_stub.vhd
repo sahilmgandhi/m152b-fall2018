0ed193efb2c232f70003e732766054c70b7ec329
Jiwan Kang <jiwanjk@ucla.edu>
2018-11-02 13:46:14 -0700

    init project 4

---
+++ Lab4/hdl/system_stub.vhd
@@ -0,0 +1,119 @@
+-------------------------------------------------------------------------------
+-- system_stub.vhd
+-------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+
+entity system_stub is
+  port (
+    fpga_0_LEDs_8Bit_GPIO_IO_pin : inout std_logic_vector(0 to 7);
+    fpga_0_Push_Buttons_7Bit_GPIO_IO_pin : inout std_logic_vector(0 to 6);
+    fpga_0_DIP_Switches_8Bit_GPIO_IO_pin : inout std_logic_vector(0 to 7);
+    fpga_0_FLASH_Mem_A_pin : out std_logic_vector(7 to 30);
+    fpga_0_FLASH_Mem_RPN_pin : out std_logic;
+    fpga_0_FLASH_Mem_CEN_pin : out std_logic;
+    fpga_0_FLASH_Mem_OEN_pin : out std_logic;
+    fpga_0_FLASH_Mem_WEN_pin : out std_logic;
+    fpga_0_FLASH_Mem_DQ_pin : inout std_logic_vector(0 to 15);
+    fpga_0_DDR2_SDRAM_DDR2_Clk_pin : out std_logic_vector(1 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_Clk_n_pin : out std_logic_vector(1 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_CE_pin : out std_logic_vector(1 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_CS_n_pin : out std_logic_vector(1 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_ODT_pin : out std_logic_vector(1 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_RAS_n_pin : out std_logic;
+    fpga_0_DDR2_SDRAM_DDR2_CAS_n_pin : out std_logic;
+    fpga_0_DDR2_SDRAM_DDR2_WE_n_pin : out std_logic;
+    fpga_0_DDR2_SDRAM_DDR2_BankAddr_pin : out std_logic_vector(1 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_Addr_pin : out std_logic_vector(12 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_DQ_pin : inout std_logic_vector(63 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_DM_pin : out std_logic_vector(7 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_DQS_pin : inout std_logic_vector(7 downto 0);
+    fpga_0_DDR2_SDRAM_DDR2_DQS_n_pin : inout std_logic_vector(7 downto 0);
+    fpga_0_RS232_Uart_0_sin_pin : in std_logic;
+    fpga_0_RS232_Uart_0_sout_pin : out std_logic;
+    fpga_0_RS232_Uart_1_sin_pin : in std_logic;
+    fpga_0_RS232_Uart_1_sout_pin : out std_logic;
+    fpga_0_clk_1_sys_clk_pin : in std_logic;
+    fpga_0_rst_1_sys_rst_pin : in std_logic
+  );
+end system_stub;
+
+architecture STRUCTURE of system_stub is
+
+  component system is
+    port (
+      fpga_0_LEDs_8Bit_GPIO_IO_pin : inout std_logic_vector(0 to 7);
+      fpga_0_Push_Buttons_7Bit_GPIO_IO_pin : inout std_logic_vector(0 to 6);
+      fpga_0_DIP_Switches_8Bit_GPIO_IO_pin : inout std_logic_vector(0 to 7);
+      fpga_0_FLASH_Mem_A_pin : out std_logic_vector(7 to 30);
+      fpga_0_FLASH_Mem_RPN_pin : out std_logic;
+      fpga_0_FLASH_Mem_CEN_pin : out std_logic;
+      fpga_0_FLASH_Mem_OEN_pin : out std_logic;
+      fpga_0_FLASH_Mem_WEN_pin : out std_logic;
+      fpga_0_FLASH_Mem_DQ_pin : inout std_logic_vector(0 to 15);
+      fpga_0_DDR2_SDRAM_DDR2_Clk_pin : out std_logic_vector(1 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_Clk_n_pin : out std_logic_vector(1 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_CE_pin : out std_logic_vector(1 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_CS_n_pin : out std_logic_vector(1 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_ODT_pin : out std_logic_vector(1 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_RAS_n_pin : out std_logic;
+      fpga_0_DDR2_SDRAM_DDR2_CAS_n_pin : out std_logic;
+      fpga_0_DDR2_SDRAM_DDR2_WE_n_pin : out std_logic;
+      fpga_0_DDR2_SDRAM_DDR2_BankAddr_pin : out std_logic_vector(1 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_Addr_pin : out std_logic_vector(12 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_DQ_pin : inout std_logic_vector(63 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_DM_pin : out std_logic_vector(7 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_DQS_pin : inout std_logic_vector(7 downto 0);
+      fpga_0_DDR2_SDRAM_DDR2_DQS_n_pin : inout std_logic_vector(7 downto 0);
+      fpga_0_RS232_Uart_0_sin_pin : in std_logic;
+      fpga_0_RS232_Uart_0_sout_pin : out std_logic;
+      fpga_0_RS232_Uart_1_sin_pin : in std_logic;
+      fpga_0_RS232_Uart_1_sout_pin : out std_logic;
+      fpga_0_clk_1_sys_clk_pin : in std_logic;
+      fpga_0_rst_1_sys_rst_pin : in std_logic
+    );
+  end component;
+
+  attribute BOX_TYPE : STRING;
+  attribute BOX_TYPE of system : component is "user_black_box";
+
+begin
+
+  system_i : system
+    port map (
+      fpga_0_LEDs_8Bit_GPIO_IO_pin => fpga_0_LEDs_8Bit_GPIO_IO_pin,
+      fpga_0_Push_Buttons_7Bit_GPIO_IO_pin => fpga_0_Push_Buttons_7Bit_GPIO_IO_pin,
+      fpga_0_DIP_Switches_8Bit_GPIO_IO_pin => fpga_0_DIP_Switches_8Bit_GPIO_IO_pin,
+      fpga_0_FLASH_Mem_A_pin => fpga_0_FLASH_Mem_A_pin,
+      fpga_0_FLASH_Mem_RPN_pin => fpga_0_FLASH_Mem_RPN_pin,
+      fpga_0_FLASH_Mem_CEN_pin => fpga_0_FLASH_Mem_CEN_pin,
+      fpga_0_FLASH_Mem_OEN_pin => fpga_0_FLASH_Mem_OEN_pin,
+      fpga_0_FLASH_Mem_WEN_pin => fpga_0_FLASH_Mem_WEN_pin,
+      fpga_0_FLASH_Mem_DQ_pin => fpga_0_FLASH_Mem_DQ_pin,
+      fpga_0_DDR2_SDRAM_DDR2_Clk_pin => fpga_0_DDR2_SDRAM_DDR2_Clk_pin,
+      fpga_0_DDR2_SDRAM_DDR2_Clk_n_pin => fpga_0_DDR2_SDRAM_DDR2_Clk_n_pin,
+      fpga_0_DDR2_SDRAM_DDR2_CE_pin => fpga_0_DDR2_SDRAM_DDR2_CE_pin,
+      fpga_0_DDR2_SDRAM_DDR2_CS_n_pin => fpga_0_DDR2_SDRAM_DDR2_CS_n_pin,
+      fpga_0_DDR2_SDRAM_DDR2_ODT_pin => fpga_0_DDR2_SDRAM_DDR2_ODT_pin,
+      fpga_0_DDR2_SDRAM_DDR2_RAS_n_pin => fpga_0_DDR2_SDRAM_DDR2_RAS_n_pin,
+      fpga_0_DDR2_SDRAM_DDR2_CAS_n_pin => fpga_0_DDR2_SDRAM_DDR2_CAS_n_pin,
+      fpga_0_DDR2_SDRAM_DDR2_WE_n_pin => fpga_0_DDR2_SDRAM_DDR2_WE_n_pin,
+      fpga_0_DDR2_SDRAM_DDR2_BankAddr_pin => fpga_0_DDR2_SDRAM_DDR2_BankAddr_pin,
+      fpga_0_DDR2_SDRAM_DDR2_Addr_pin => fpga_0_DDR2_SDRAM_DDR2_Addr_pin,
+      fpga_0_DDR2_SDRAM_DDR2_DQ_pin => fpga_0_DDR2_SDRAM_DDR2_DQ_pin,
+      fpga_0_DDR2_SDRAM_DDR2_DM_pin => fpga_0_DDR2_SDRAM_DDR2_DM_pin,
+      fpga_0_DDR2_SDRAM_DDR2_DQS_pin => fpga_0_DDR2_SDRAM_DDR2_DQS_pin,
+      fpga_0_DDR2_SDRAM_DDR2_DQS_n_pin => fpga_0_DDR2_SDRAM_DDR2_DQS_n_pin,
+      fpga_0_RS232_Uart_0_sin_pin => fpga_0_RS232_Uart_0_sin_pin,
+      fpga_0_RS232_Uart_0_sout_pin => fpga_0_RS232_Uart_0_sout_pin,
+      fpga_0_RS232_Uart_1_sin_pin => fpga_0_RS232_Uart_1_sin_pin,
+      fpga_0_RS232_Uart_1_sout_pin => fpga_0_RS232_Uart_1_sout_pin,
+      fpga_0_clk_1_sys_clk_pin => fpga_0_clk_1_sys_clk_pin,
+      fpga_0_rst_1_sys_rst_pin => fpga_0_rst_1_sys_rst_pin
+    );
+
+end architecture STRUCTURE;
+
---
+++ Lab4/mariokart/Debug/src/platform.d
@@ -0,0 +1,34 @@
+src/platform.d: ../src/platform.c \
+ ../../mariokart_bsp/microblaze_0/include/xparameters.h \
+ ../../mariokart_bsp/microblaze_0/include/xil_cache.h \
+ ../../mariokart_bsp/microblaze_0/include/mb_interface.h \
+ ../../mariokart_bsp/microblaze_0/include/xbasic_types.h \
+ ../../mariokart_bsp/microblaze_0/include/xil_types.h \
+ ../../mariokart_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h \
+ ../../mariokart_bsp/microblaze_0/include/xuartns550_l.h \
+ ../../mariokart_bsp/microblaze_0/include/xil_assert.h \
+ ../../mariokart_bsp/microblaze_0/include/xil_io.h \
+ ../../mariokart_bsp/microblaze_0/include/xil_printf.h
+
+../../mariokart_bsp/microblaze_0/include/xparameters.h:
+
+../../mariokart_bsp/microblaze_0/include/xil_cache.h:
+
+../../mariokart_bsp/microblaze_0/include/mb_interface.h:
+
+../../mariokart_bsp/microblaze_0/include/xbasic_types.h:
+
+../../mariokart_bsp/microblaze_0/include/xil_types.h:
+
+../../mariokart_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
+
+../../mariokart_bsp/microblaze_0/include/xuartns550_l.h:
+
+../../mariokart_bsp/microblaze_0/include/xil_assert.h:
+
+../../mariokart_bsp/microblaze_0/include/xil_io.h:
+
+../../mariokart_bsp/microblaze_0/include/xil_printf.h:
