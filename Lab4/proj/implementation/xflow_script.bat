622b9ede3dd8b495f9d520aff1e8ddd83af390c1
Jiwan Kang <jiwanjk@ucla.edu>
2018-11-14 10:54:44 -0800

    new mariokart

--- Lab4/proj/implementation/xflow_script.bat
+++ Lab4/proj/implementation/xflow_script.bat
@@ -8,18 +8,3 @@
 @REM #
 ngdbuild -p xc5vlx50tff1136-1 -nt timestamp -bm system.bmm "C:/Users/CS152B/Documents/m152b_fall2018/Lab4/proj/implementation/system.ngc" -uc system.ucf system.ngd 
 
-@REM #
-@REM # Command line for map
-@REM #
-map -o system_map.ncd -w -pr b -ol high -timing -detail system.ngd system.pcf 
-
-@REM #
-@REM # Command line for par
-@REM #
-par -w -ol high system_map.ncd system.ncd system.pcf 
-
-@REM #
-@REM # Command line for post_par_trce
-@REM #
-trce -e 3 -xml system.twx system.ncd system.pcf 
-
---
+++ Mariokart/HelloCamera/Debug/src/main.d
@@ -0,0 +1,18 @@
+src/main.d: ../src/main.c \
+ ../../HelloCamera_bsp/microblaze_0/include/xio.h \
+ ../../HelloCamera_bsp/microblaze_0/include/xbasic_types.h \
+ ../../HelloCamera_bsp/microblaze_0/include/xparameters.h \
+ ../src/cam_ctrl_header.h ../src/vmodcam_header.h \
+ ../../HelloCamera_bsp/microblaze_0/include/xstatus.h
+
+../../HelloCamera_bsp/microblaze_0/include/xio.h:
+
+../../HelloCamera_bsp/microblaze_0/include/xbasic_types.h:
+
+../../HelloCamera_bsp/microblaze_0/include/xparameters.h:
+
+../src/cam_ctrl_header.h:
+
+../src/vmodcam_header.h:
+
+../../HelloCamera_bsp/microblaze_0/include/xstatus.h:
