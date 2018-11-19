922f27c1ca9298f7233923053f8e5c9aa0244206
Jiwan Kang <jiwanjk@ucla.edu>
2018-11-19 15:38:28 -0800

    Display working again

--- mariokart2/mario/Debug/src/main.d
+++ mariokart2/mario/Debug/src/main.d
@@ -13,7 +13,8 @@ src/main.d: ../src/main.c ../../mario_bsp/microblaze_0/include/xio.h \
  ../../mario_bsp/microblaze_0/include/xparameters.h \
  ../src/cam_ctrl_header.h ../src/vmodcam_header.h \
  ../../mario_bsp/microblaze_0/include/xstatus.h ../src/pmodGYRO.h \
- ../../mario_bsp/microblaze_0/include/xil_types.h
+ ../../mario_bsp/microblaze_0/include/xil_types.h ../src/game_board.h \
+ ../src/globals.h ../src/game_controller.h ../src/display.h
 
 ../../mario_bsp/microblaze_0/include/xio.h:
 
@@ -54,3 +55,11 @@ src/main.d: ../src/main.c ../../mario_bsp/microblaze_0/include/xio.h \
 ../src/pmodGYRO.h:
 
 ../../mario_bsp/microblaze_0/include/xil_types.h:
+
+../src/game_board.h:
+
+../src/globals.h:
+
+../src/game_controller.h:
+
+../src/display.h:
